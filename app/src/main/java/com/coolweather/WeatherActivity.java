58189813ec493757754727ad69b3030694c4fe9e
jiao <jiaojiaoas007@gmail.com>
2017-08-10 18:03:43 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/WeatherActivity.java
+++ app/src/main/java/com/coolweather/WeatherActivity.java
@@ -152,6 +152,7 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
+        weatherLayout.setVisibility(View.VISIBLE);
     }
 
     /**
